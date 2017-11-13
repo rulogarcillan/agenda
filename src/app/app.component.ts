2ca77172b1fe4c52b2dd0f2d136cbbb088432935
rulogarcillan <raulrcs@gmail.com>
2017-11-13 23:40:03 +0100

    Implementamos hook

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
+import { OnInit } from '@angular/core/src/metadata/lifecycle_hooks';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+  ngOnInit(): void {
+    console.log('Hola agenda');
+  }
   title = 'Agenda';
+
 }
